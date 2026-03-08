a652035d4a1a67c81b7d5c76e9d1ea68748d69c2
Kostiantyn Burnazaki <kostas.burnazaki@gmail.com>
2026-03-08 12:30:44 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def main() -> None:
+    book = load_data()
+
+    print("Welcome to the assistant bot!")
+
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ("close", "exit"):
+            save_data(book)
+            print("Good bye!")
+            break
+
+        if command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, book))
+        elif command == "change":
+            print(change_contact(args, book))
+        elif command == "phone":
+            print(show_phone(args, book))
+        elif command == "all":
+            print(show_all(book))
+        else:
+            print("Invalid command.")
